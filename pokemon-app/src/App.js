0315110bda1ff6b7231185d1505196b22f6115d2
watttaraaru <101541944+wattaraaru@users.noreply.github.com>
2023-01-30 16:36:58 +0900

    URLから情報を取得

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLiading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      setLiading(false);
+    };
+    fetchPokemonData();
+  },[]);
   return (
-    <div className="App"> </div>
+    <div className="App">
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ):(
+      <>
+      <h1>ポケモンデータを取得しました</h1>
+      </>
+    )}
+    </div>
   );
 }
 
